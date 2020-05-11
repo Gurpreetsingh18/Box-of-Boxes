bb7b75270f6822bad091497d23d06b25afb8a794
Gurpreetsingh18 <61836288+Gurpreetsingh18@users.noreply.github.com>
2020-05-11 10:10:21 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,21 +16,18 @@ function setup() {
     gSlider = createSlider(0, 100, 50);
     gSlider.position(40, 365);
     gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1);
-    ground = new Ground(400,390,400,20); 
+    ground = new Ground(200,360,400,20); 
 
 }
 
 function mousePressed() {
     if (mouseY < 350) {
-       box = new Box(100,200,70,70);   
+        boxes.push(new Box(mouseX, mouseY, random(10, 40), random(10, 40)));  
     }
 }
- 
+
 function draw() {
-    Engine.update(engine);
-    background(0);
-    ground.display();
-    box.display();
+    background(51);
     var fVal = gSlider.value();
  
     for (var i = 0; i < boxes.length; i++) {
@@ -40,16 +37,29 @@ function draw() {
     fill(170);
     strokeWeight(4);
     rectMode(CENTER);
-    rect(ground.position.x, ground.position.y, width, 10);
+    rect(ground.body.position.x, ground.body.position.y, width, 10);
     fill(255);
     textSize(15);
     text("Gravity " + fVal, 160, 381);
+    text("Press Mouse To Create A Box",50,40);
 }
+
+
+function Box(x, y, w, h, options) {
+    var options = {
+        friction: 0.5,
+        restitution: 0.5,
+    }
+ 
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    World.add(world, this.body);
  
     this.show = function () {
         var pos = this.body.position;
         var angle = this.body.angle;
- 
+
         push();
         translate(pos.x, pos.y);
         rotate(angle);
@@ -57,6 +67,7 @@ function draw() {
         strokeWeight(1);
         stroke(255);
         fill(127);
-        rect(0, 0, this.width, this.height);
+        rect(0, 0, this.w, this.h);
         pop();
     }
+}
